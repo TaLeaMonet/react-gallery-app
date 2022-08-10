4deddf44674aa689016176777762713630499805
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-08-10 15:37:19 -0700

    Fix link styling. Remove unused code.

--- react_gallery_app-v1/gallery-app/src/App.js
+++ react_gallery_app-v1/gallery-app/src/App.js
@@ -1,5 +1,5 @@
 import apiKey from './config';
-import { BrowserRouter, 
+import {  
          Switch,
          Route,
          withRouter,
@@ -8,29 +8,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Search from './components/Search';
 import PhotoResults from './components/PhotoResults';
-import Photos from './components/Photos';
-import NotFound from './components//NotFound';
 import React, { Component } from 'react';
-
-
-
-
-/* Fetching data current approach drawbacks and potential approach 
-
-Currently the handleGetRequest function is set up to take in an event object.
- This causes it to not be very reusable and only usable in search upon 
- submitting the search request, so when calling it in componentDidMount 
- lifecycle method on line 29 e is undefined.
-
-Strip this function down a bit so that it takes in a searchTerm parameter
- instead of an event. Then when calling the function in componentDidMount, 
- pass it a string with a searchTerm.
-
-To get the search functionality working, create a new function in Search 
-component that does use the event object to prevent the default and then 
-makes a call to the handleGetRequest function while passing it the value 
-that the user has provided.
-*/
 
 class App extends Component {
 
@@ -47,7 +25,7 @@ class App extends Component {
   if(e) {
    e.preventDefault()
   
-   const searchTerm = e.target.elements.searchValue.value
+   const searchTerm = e.target[0].value
    this.setState({searchTitle: searchTerm})
    const data = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchTerm}&per_page=24&format=json&nojsoncallback=1`
    const request = await fetch(data)
@@ -102,6 +80,7 @@ componentDidMount() {
               <Route exact path='/dogs' render={() => (<PhotoResults images={this.state.dogs} title='Dogs' />)} />
               <Route exact path='/computers' render={() => (<PhotoResults images={this.state.computers} title='Computers' />)} />
               <Route path='/:id'  render={() => (<PhotoResults images={this.state.images} title={this.state.searchTitle} />)}/>
+   
           </Switch>
        </div>
 
--- react_gallery_app-v1/gallery-app/src/components/Nav.js
+++ react_gallery_app-v1/gallery-app/src/components/Nav.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Router, Switch } from  "react-router-dom";
-import PhotoResults from './PhotoResults';
-import Photos from './Photos';
-import Container from 'react-bootstrap/Container';
+import {  NavLink } from  "react-router-dom";
 
 class Nav extends Component {
   render () {
   return (
     <nav className="main-nav">
         <ul>
-          <li><NavLink to={'/cats'}>Cats</NavLink></li>
-          <li><NavLink to={'/dogs'}>Dogs</NavLink></li>
-          <li><NavLink to={'/computers'}>Computers</NavLink></li>
+          <li><NavLink className="nav-link" to={'/cats'}>Cats</NavLink></li>
+          <li><NavLink className="nav-link"  to={'/dogs'}>Dogs</NavLink></li>
+          <li><NavLink className="nav-link" to={'/computers'}>Computers</NavLink></li>
         </ul>  
     </nav>
 
